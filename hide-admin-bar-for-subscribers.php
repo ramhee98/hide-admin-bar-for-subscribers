5180e26dee8ed8241211fd6ef24802b62ccab012
ramhee98 <heeb-ramon@hotmail.com>
2023-03-08 13:44:50 +0100

    initial version

--- hide-admin-bar-for-subscribers.php
+++ hide-admin-bar-for-subscribers.php
@@ -1,1 +1,20 @@
+<?php
+/**
+* Plugin Name: Hide Admin Bar for Subscribers
+* Plugin URI: https://github.com/ramhee98/hide-admin-bar-for-subscribers
+* Description: This Plugin hides the Admin Bar on top for subscribers.
+* Version: 1.0
+* Author: ramhee
+* Author URI: https://ramhee.ch/
+* License: GPL-3.0
+* License URI: https://github.com/ramhee98/hide-admin-bar-for-subscribers/blob/main/LICENSE
+*/
 
+add_action('after_setup_theme', 'remove_admin_bar');
+ 
+function remove_admin_bar() {
+	if (current_user_can('subscriber')) //Hide admin bar if user is a subscriber
+	{
+		show_admin_bar(false);
+	}
+}
